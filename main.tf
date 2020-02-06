0096d0649a86473aa6f7a64bc3fb7ce45f62b78b
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2020-02-06 12:48:55 +0100

    Add basic lifecycle based on images count and tag prefixes

--- main.tf
+++ main.tf
@@ -26,3 +26,27 @@ resource "aws_ecr_repository_policy" "allow_account_pull" {
   policy = data.aws_iam_policy_document.allow_account_pull.json
 }
 
+resource "aws_ecr_lifecycle_policy" "basic" {
+  repository = aws_ecr_repository.this.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep last ${var.images_retention_count} images with tag prefix ${replace(join(",", var.images_retention_tag_prefix_list), ",", " and ")}",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ${jsonencode(var.images_retention_tag_prefix_list)},
+        "countType": "imageCountMoreThan",
+        "countNumber": ${var.images_retention_count}
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
